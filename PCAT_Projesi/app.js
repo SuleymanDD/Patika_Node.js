0233767bc923f576423a06d91fddb9a7a81f9bf0
SuleymanDD <sdemirel.2375@gmail.com>
2025-08-22 19:23:08 +0300

    Changes for PCAT_Projesi
    -Ejs lib imported to project for Template Engine.
    -A folder named partials was created.
    -Patrials folder uses for repeated codes.
    -The folder named temp has been changed to views.

--- PCAT_Projesi/app.js
+++ PCAT_Projesi/app.js
@@ -1,13 +1,24 @@
 
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs')
 const app = express();
 
 app.use(express.static('public'));
+app.set("view engine","ejs");
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index');
 });
+app.get('/about', (req,res) => {
+    res.render('about');
+});
+app.get('/addPhoto', (req,res) => {
+    res.render('addPhoto');
+});
+
+
+
 
 
 
