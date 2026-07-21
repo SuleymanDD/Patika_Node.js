0e933277aa191918fe5db8ceac8cb4b98f4b8112
SuleymanDD <sdemirel.2375@gmail.com>
2026-07-21 19:42:44 +0300

    Active page was shown on navbar and footer

--- Fixtures_Project/controllers/pageController.js
+++ Fixtures_Project/controllers/pageController.js
@@ -1,16 +1,16 @@
 
 exports.getHomePage = (req, res) => {
-    res.render("index");
+    res.render("index", {pageName: "home"});
 }
 
 exports.getAboutPage = (req, res) => {
-    res.render("about");
+    res.render("about", {pageName: "about"});
 }
 
 exports.getFurnituresPage = (req, res) => {
-    res.render("furnitures");
+    res.render("furnitures", {pageName: "furnitures"});
 }
 
 exports.getContactPage = (req, res) => {
-    res.render("contact");
+    res.render("contact", {pageName: "contact"});
 }
